d5a2f01900b445db398a0ba9f1ecf1e5fee47de3
Leooa501 <leon.sas012@gmail.com>
2026-03-17 13:26:53 +0100

    v.1.0.1.2

--- WILK/obj/Release/net8.0-windows/WILK.AssemblyInfo.cs
+++ WILK/obj/Release/net8.0-windows/WILK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WILK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a6b4f1f0e4e5b76158a06014f07addaff58b5c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c46d58a2c035c5998801e51bde24694ce68aa19")]
 [assembly: System.Reflection.AssemblyProductAttribute("WILK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WILK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
